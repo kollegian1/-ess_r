4668be0a56aed73494b70c32c680269f12a97f02
kollegian1 <121588287+kollegian1@users.noreply.github.com>
2023-01-07 23:39:04 +0100

    Add files via upload

---
+++ client/setup.py
@@ -0,0 +1,15 @@
+LITECOINIMPORT = 'FROM_LIB_IMPORTLITECOIN_FLUCTUATION'
+# ENTER THE API USERHAPS YOU HAVE GENERATED, BE AWARE YOU SHOULD WAIT 48H BEFORE THOSE ARE ACTIVATED AFTER HAVING CREATED THEM AT https://www.coinbase.com/settings/api .
+first = 'Replace_This_With_Your_APIKY'
+second = 'Replace_This_With_Your_Longer_APIKY' 
+
+third = 'blockchainlib_BTCFluctuation_listener_Import_As:BTCPROFITS'
+
+# Using a different method to import $US.000 accurate prices to avoid invalid callback :
+
+fourth = 'FROM_LIB_IMPORT_ETHNODEPRICES'
+fifth = 'FROM_LIB_IMPORT_APECOIN_FLUCTUATION_PRICES'
+# ApeCoin Fluctuation Prices may sometimes help predict the movements of LITECOIN, which then can be used to make short/long calls.
+
+
+
